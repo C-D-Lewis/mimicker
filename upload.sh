e792dac2593a384935ff0281c6049faca21f3648
C-D-Lewis <Bonsitm@gmail.com>
2020-11-05 12:47:15 +0000

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -8,7 +8,7 @@ REGION=us-east-1
 rm -rf build
 
 mkdir -p build
-zip -r9 build/bundle.zip quotes.txt index.js node_modules
+zip -r9 build/bundle.zip index.js node_modules
 
 aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb://build/bundle.zip  --region $REGION
 
